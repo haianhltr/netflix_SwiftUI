526f4a542385da15f0daadc249971a399a1c03d4
Anh Le <haianhltr@yahoo.com.vn>
2022-04-20 23:00:37 -0500

    Initial Commit

---
+++ netflix/netflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  netflixApp.swift
+//  netflix
+//
+//  Created by Anh Le Truong on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct netflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
